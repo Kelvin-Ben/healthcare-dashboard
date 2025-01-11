a2f975fb7cada5fd40008ec927b9c34fcc76f7c8
Kelvin-Ben <kevoben14@gmail.com>
2025-01-11 17:31:20 +0300

    working on helper function to display levels

--- src/modules/helper.js
+++ src/modules/helper.js
@@ -27,70 +27,75 @@ export function handleTestClicks() {
 // Helper function to calculate average and determine
 const calculateLevel = (value, type) => {
   if (type === 'systolic') {
-    if (value < 120) return {level: "Normal", class: 'normal'};
-    if (value < 140) return {level: "Elevated", class: 'elevated'};
-    return { level: "Higher than Average", class: "high"};
+    if (value < 120) return {level: "Lower than Average"};
+    if (value > 140) return {level: "Higher than Average"};
+    return { level: "Normal"};
   } else if (type === 'diastolic') {
-    if (value < 80) return {level: "Normal", class: 'normal'};
-    if (value < 90) return {level: "Elevated", class: 'elevated'};
-    return { level: "Higher than Average", class: "high"};
-  } else if (type === 'diastolic') {
-    if (value < 80) return {level: "Normal", class: 'normal'};
-    if (value < 90) return {level: "Elevated", class: 'elevated'};
-    return { level: "Higher than Average", class: "high"};
+    if (value < 80) return {level: "Lower than Average"};
+    if (value > 90) return {level: "Higher than Average"};
+    return { level: "Normal"};
   } else if (type === 'respiratory') {
-    if (value < 16) return {level: 'Lower than Average', class: 'low'};
-    if (value <= 20) return {level: 'Normal', class: 'normal'};
-    return { level: "Lower than Average", class: 'low'}
+    if (value < 16) return {level: "Lower than Average"};
+    if (value <= 20) return {level: "Higher than Average"};
+    return { level: "Normal"};
   } else if (type === 'temperature') {
-    if (value < 97.7) return {level: 'Lower than Average', class: 'low'};
-    if (value <= 99.5) return {level: 'Normal', class: 'normal'};
-    return { level: "Higher than Average", class: 'high'}
+    if (value < 97.5) return {level: "Lower than Average"};
+    if (value <= 99.5) return {level: "Higher than Average"};
+    return { level: "Normal"};
   } else if (type === 'heart') {
-    if (value < 60) return {level: 'Lower than Average', class: 'low'};
-    if (value <= 100) return {level: 'Higher than Average', class: 'high'};
-    return { level: "Higher than Average", class: 'high'}
+    if (value < 60) return {level: "Lower than Average"};
+    if (value > 100) return {level: "Higher than Average"};
+    return { level: "Normal"};
   }
 }
 
 // Helper function to update HTML section
 export const updateInfoSections = (filteredHistory) => {
-  const systolicAvg = Math.round(
-    filteredHistory.reduce((sum, item) => sum + item.blood_pressure.systolic.value, 0)  / filteredHistory.length
-  );
-  const diastolicAvg = Math.round(
-    filteredHistory.reduce((sum, item) => sum + item.blood_pressure.diastolic.value, 0)  / filteredHistory.length
-  );
-  
-  const systolicLevel = calculateLevel(systolicAvg, "systolic")
-  const diastolicLevel = calculateLevel(diastolicAvg, "diastolic")
-  
-  // Update the systolic and diastolic info
-  document.querySelector('.systolic .value').textContent = systolicAvg;
-  document.querySelector('.systolic .level').textContent = systolicLevel;
-  document.querySelector('.diastolic .value').textContent = diastolicAvg;
-  document.querySelector('.diastolic .level').textContent = diastolicLevel;
+  const systolicData = filteredHistory.map((record) => record.blood_pressure.systolic.value);
+  console.log(systolicData)
 
-  // update respiratory rate
-  const respRateAvg = Math.round(
-    filteredHistory.reduce((sum, item) => sum + item.respiratory_rate.value, 0) / filteredHistory.length
-  )
-  const respRateLevel = calculateLevel(respRateAvg, 'respiratory')
-  document.querySelector('.respiratory-rate .bpm').textContent = `${respRateAvg} bpm`
-  document.querySelector('.respiratory-rate .level').textContent = respRateLevel
+  /**
+   * NOTE:- This function needs more concentration, am trying to get the levels display
+   */
+
+    const systolicAvg = Math.round(
+      systolicData.reduce((sum, item) => sum + item, 0)  / filteredHistory.length
+    );
+    const diastolicAvg = Math.round(
+      filteredHistory.reduce((sum, item) => sum + item.blood_pressure.diastolic.value, 0)  / filteredHistory.length
+    );
+    console.log(filteredHistory)
+    // console.log(filteredHistory.blood_pressure.systolic.value)
+    
+    const systolicLevel = calculateLevel(systolicAvg, "systolic")
+    const diastolicLevel = calculateLevel(diastolicAvg, "diastolic")
+    console.log(systolicLevel, diastolicLevel)
+    // Update the systolic and diastolic info
+    document.querySelector('.systolic .value').textContent = systolicAvg;
+    document.querySelector('.systolic .level').textContent = systolicLevel;
+    document.querySelector('.diastolic .value').textContent = diastolicAvg;
+    document.querySelector('.diastolic .level').textContent = diastolicLevel;
   
-  const tempAvg = Math.round(
-    filteredHistory.reduce((sum, item) => sum + item.temperature.value, 0) / filteredHistory.length
-  ).toFixed(1)
-  const tempLevel = calculateLevel(tempAvg, 'temperature')
-
-  document.querySelector('#fahrenheit').innerHTML = `${tempAvg} <sup>0</sup> F`
-  document.querySelector('.temperature .level').innerHTML = tempLevel
-
-  const heartAvg = Math.round(
-    filteredHistory.reduce((sum, item) => sum + item.heart_rate.value, 0) / filteredHistory.length
-  )
-  const heartLevel = calculateLevel(heartAvg, 'heart')
-  document.querySelector('.heart-rate .bpm').innerHTML = `${heartAvg} bpm`
-  console.log(document.querySelector('.heart-rate .level').textContent = heartLevel)
+    // update respiratory rate
+    const respRateAvg = Math.round(
+      filteredHistory.reduce((sum, item) => sum + item.respiratory_rate.value, 0) / filteredHistory.length
+    )
+    const respRateLevel = calculateLevel(respRateAvg, 'respiratory')
+    document.querySelector('.respiratory-rate .bpm').textContent = `${respRateAvg} bpm`
+    document.querySelector('.respiratory-rate .level').textContent = respRateLevel
+    
+    const tempAvg = Math.round(
+      filteredHistory.reduce((sum, item) => sum + item.temperature.value, 0) / filteredHistory.length
+    ).toFixed(1)
+    const tempLevel = calculateLevel(tempAvg, 'temperature')
+  
+    document.querySelector('#fahrenheit').innerHTML = `${tempAvg} <sup>0</sup> F`
+    document.querySelector('.temperature .level').innerHTML = tempLevel
+  
+    const heartAvg = Math.round(
+      filteredHistory.reduce((sum, item) => sum + item.heart_rate.value, 0) / filteredHistory.length
+    )
+    const heartLevel = calculateLevel(heartAvg, 'heart')
+    document.querySelector('.heart-rate .bpm').innerHTML = `${heartAvg} bpm`
+    console.log(document.querySelector('.heart-rate .level').textContent = heartLevel)
 }
