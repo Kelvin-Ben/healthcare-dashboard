b23fef4d674b363003fbe034d1dea87729c7ccdd
Kelvin-Ben <kevoben14@gmail.com>
2025-01-14 18:32:31 +0300

    Finished dashboard functionalities

--- src/modules/chart.js
+++ src/modules/chart.js
@@ -1,95 +1,99 @@
-import { Chart } from "chart.js/auto"
+import { Chart } from "chart.js/auto";
 import { updateInfoSections } from "./helper";
 
 let bloodPressureChartInstance = null;
 
 export const renderChart = (data) => {
-  // data.forEach((patient) => {
-    const durationSelect = document.getElementById('month-range');
-    
-    const createChart = (patient, months) => {
-      console.log(patient)
-      const diagnosisHistory = patient.diagnosis_history
-      const filteredHistory = diagnosisHistory.slice(-months).reverse();
-      // prepare data for the chart
-      const labels = filteredHistory.map(item => `${(item.month).slice(0, 3)}, ${item.year}`);
-      const systolicData = filteredHistory.map(item => item.blood_pressure.systolic.value);
-      const diastolicData = filteredHistory.map(item =>item.blood_pressure.diastolic.value);
-  
-      // get chart context
-      const bloodPressureChart = document.querySelector('#blood-pressure-chart');
-      const ctx = bloodPressureChart.getContext('2d');
-  
-      // Destroy existing chart instance if it exists
-      if (bloodPressureChartInstance) {
-        bloodPressureChartInstance.destroy();
-      }
-  
-      bloodPressureChartInstance = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels,
-          datasets: [
-            {
-              label: "Systolic",
-              data: systolicData,
-              borderColor: '#E66FD2',
-              backgroundColor: '#E66FD2',
-              borderWidth: 2,
-            },
-            {
-              label: "Diastolic",
-              data: diastolicData,
-              borderColor: "#8C6FE6",
-              backgroundColor: "#8C6FE6",
-              borderWidth: 2,
-            }
-          ]
+  const durationSelect = document.getElementById("month-range");
+
+  const createChart = (patient, months) => {
+    const diagnosisHistory = patient.diagnosis_history;
+    const filteredHistory = diagnosisHistory.slice(-months).reverse();
+
+    // prepare data for the chart
+    const labels = filteredHistory.map(
+      (item) => `${item.month.slice(0, 3)}, ${item.year}`
+    );
+    const systolicData = filteredHistory.map(
+      (item) => item.blood_pressure.systolic.value
+    );
+    const diastolicData = filteredHistory.map(
+      (item) => item.blood_pressure.diastolic.value
+    );
+
+    // get chart context
+    const bloodPressureChart = document.querySelector("#blood-pressure-chart");
+    const ctx = bloodPressureChart.getContext("2d");
+
+    // Destroy existing chart instance if it exists
+    if (bloodPressureChartInstance) {
+      bloodPressureChartInstance.destroy();
+    }
+
+    bloodPressureChartInstance = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Systolic",
+            data: systolicData,
+            borderColor: "#E66FD2",
+            backgroundColor: "#E66FD2",
+            borderWidth: 2,
+          },
+          {
+            label: "Diastolic",
+            data: diastolicData,
+            borderColor: "#8C6FE6",
+            backgroundColor: "#8C6FE6",
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
         },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: false,
+              text: "Blood Pressure (mmHg)",
+            },
+          },
+          x: {
+            title: {
+              display: false,
+              text: "Months",
+            },
+            grid: {
               display: false,
             },
           },
-          scales: {
-            y: {
-              beginAtZero: true,
-              title: {
-                display: false,
-                text: "Blood Pressure (mmHg)"
-              },
-            },
-            x: {
-              title: {
-                display: false,
-                text: "Months"
-              },
-              grid: {
-                display: false
-              }
-            }
+        },
+        elements: {
+          line: {
+            tension: 0.4,
           },
-          elements: {
-            line: {
-              tension: 0.4
-            }
-          }
-        }
-      })
-      updateInfoSections(filteredHistory)
-    }
-    // render the inital chart for 6 months
-    const initialMonths = 6;
-    createChart(data[0], initialMonths)
+        },
+      },
+    });
+    updateInfoSections(filteredHistory);
+  };
+  
+  // render the inital chart for 6 months
+  const initialMonths = 6;
+  createChart(data[0], initialMonths);
 
-    // listen for changes in duration
-    durationSelect.addEventListener('change', (event) => {
-      const selectedMonths = parseInt(event.target.value, 10)
-      createChart(data[0], selectedMonths)
-    })
-
-  // })
-}
+  // listen for changes in duration
+  durationSelect.addEventListener("change", (event) => {
+    const selectedMonths = parseInt(event.target.value, 10);
+    createChart(data[0], selectedMonths);
+  });
+};
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,101 +1,155 @@
+const spinner = document.getElementById("loading-spinner");
+const container = document.querySelector(".container");
+const patientsList = document.querySelector(".patient-list");
+
+patientsList.innerHTML = "";
+
 export function handleLinkClicks() {
   const links = document.querySelectorAll(".details a");
 
-  links.forEach(link => {
-    link.addEventListener('click', function() {
-      links.forEach(link => {
-        link.classList.remove('active')
-        this.classList.add('active')
-      })
-    })
-  })
+  links.forEach((link) => {
+    link.addEventListener("click", function () {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        this.classList.add("active");
+      });
+    });
+  });
 }
 
-export function handleTestClicks() {
-  const allTests = document.querySelectorAll('.test');
-  
-  allTests.forEach(test => {
-    test.addEventListener('click', function() {
-      allTests.forEach(test => {
-        test.classList.remove('test-active');
-        this.classList.add('test-active')
+// search patient function
+export function searchPatient(event) {
+  const searchTerm = event.target.value.trim().toLowerCase();
+  const patients = document.querySelectorAll(".patient-list .patients-info");
+  patients.forEach((patient) => {
+    patient.style.display = "revert";
+    if (!patient.innerText.toLowerCase().includes(searchTerm)) {
+      patient.style.display = "none";
+    }
+  });
+}
+
+/**
+ *
+ * @param {boolean} isLoading - True to show spinner, False to hide
+ */
+export const toggleSpinner = (isLoading) => {
+  spinner.classList.toggle("hidden", !isLoading);
+  container.classList.toggle("hidden", isLoading);
+};
+
+export const renderPatientList = (data) => {
+  data.forEach((item, index) => {
+    const { name, gender, age, profile_picture } = item;
+    const patientItem = `
+      <li class="patients-info" key="${index}">
+        <div class="patients-info__details">
+          <img src="${profile_picture}" alt="Profile picture of ${profile_picture}" class="patients-img">
+          <div>
+            <p class="patients-name">${name}</p>
+            <p class="patients-details">
+              <span class="patients-gender">${gender}, </span>
+              <span class="patients-age">${age}</span>
+            </p>
+          </div>
+        </div>
+        <img src="./more_horiz_FILL0_wght300_GRAD0_opsz24.svg" alt="" class="three-dots-img">
+      </li>
+    `;
+    patientsList.insertAdjacentHTML("beforeend", patientItem);
+  });
+  const allPatients = document.querySelectorAll('.patients-info');
+  allPatients.forEach((patient) => {
+    patient.addEventListener('click', function () {
+      allPatients.forEach((patient) => {
+        patient.classList.remove('active-patient');
+        this.classList.add('active-patient')
+        console.log(this)
       })
     })
   })
-}
+};
+
 
 // Helper function to calculate average and determine
 const calculateLevel = (value, type) => {
-  if (type === 'systolic') {
-    if (value < 120) return {level: "Lower than Average"};
-    if (value > 140) return {level: "Higher than Average"};
-    return { level: "Normal"};
-  } else if (type === 'diastolic') {
-    if (value < 80) return {level: "Lower than Average"};
-    if (value > 90) return {level: "Higher than Average"};
-    return { level: "Normal"};
-  } else if (type === 'respiratory') {
-    if (value < 16) return {level: "Lower than Average"};
-    if (value <= 20) return {level: "Higher than Average"};
-    return { level: "Normal"};
-  } else if (type === 'temperature') {
-    if (value < 97.5) return {level: "Lower than Average"};
-    if (value <= 99.5) return {level: "Higher than Average"};
-    return { level: "Normal"};
-  } else if (type === 'heart') {
-    if (value < 60) return {level: "Lower than Average"};
-    if (value > 100) return {level: "Higher than Average"};
-    return { level: "Normal"};
+  if (type === "systolic") {
+    if (value < 120) return { level: "Lower than Average" };
+    if (value > 140) return { level: "Higher than Average" };
+    return { level: "Normal" };
+  } else if (type === "diastolic") {
+    if (value < 80) return { level: "Lower than Average" };
+    if (value > 90) return { level: "Higher than Average" };
+    return { level: "Normal" };
+  } else if (type === "respiratory") {
+    if (value < 16) return { level: "Lower than Average" };
+    if (value >= 20) return { level: "Higher than Average" };
+    return { level: "Normal" };
+  } else if (type === "temperature") {
+    if (value < 97.5) return { level: "Lower than Average" };
+    if (value >= 99.5) return { level: "Higher than Average" };
+    return { level: "Normal" };
+  } else if (type === "heart") {
+    if (value < 60) return { level: "Lower than Average" };
+    if (value >= 100) return { level: "Higher than Average" };
+    return { level: "Normal" };
   }
-}
+};
 
-// Helper function to update HTML section
+// Helper function to update diagnosis section
 export const updateInfoSections = (filteredHistory) => {
-  const systolicData = filteredHistory.map((record) => record.blood_pressure.systolic.value);
-  console.log(systolicData)
+  const systolicData = filteredHistory.map(
+    (record) => record.blood_pressure.systolic.value
+  );
+  const diastolicData = filteredHistory.map(
+    (record) => record.blood_pressure.diastolic.value
+  );
+  const tempData = filteredHistory.map((record) => record.temperature.value);
+  const respData = filteredHistory.map(
+    (record) => record.respiratory_rate.value
+  );
+  const heartData = filteredHistory.map((record) => record.heart_rate.value);
 
-  /**
-   * NOTE:- This function needs more concentration, am trying to get the levels display
-   */
+  const systolicAvg = Math.round(
+    systolicData.reduce((sum, value) => sum + value, 0) / filteredHistory.length
+  );
+  const diastolicAvg = Math.round(
+    diastolicData.reduce((sum, value) => sum + value, 0) /
+      filteredHistory.length
+  );
 
-    const systolicAvg = Math.round(
-      systolicData.reduce((sum, item) => sum + item, 0)  / filteredHistory.length
-    );
-    const diastolicAvg = Math.round(
-      filteredHistory.reduce((sum, item) => sum + item.blood_pressure.diastolic.value, 0)  / filteredHistory.length
-    );
-    console.log(filteredHistory)
-    // console.log(filteredHistory.blood_pressure.systolic.value)
-    
-    const systolicLevel = calculateLevel(systolicAvg, "systolic")
-    const diastolicLevel = calculateLevel(diastolicAvg, "diastolic")
-    console.log(systolicLevel, diastolicLevel)
-    // Update the systolic and diastolic info
-    document.querySelector('.systolic .value').textContent = systolicAvg;
-    document.querySelector('.systolic .level').textContent = systolicLevel;
-    document.querySelector('.diastolic .value').textContent = diastolicAvg;
-    document.querySelector('.diastolic .level').textContent = diastolicLevel;
-  
-    // update respiratory rate
-    const respRateAvg = Math.round(
-      filteredHistory.reduce((sum, item) => sum + item.respiratory_rate.value, 0) / filteredHistory.length
-    )
-    const respRateLevel = calculateLevel(respRateAvg, 'respiratory')
-    document.querySelector('.respiratory-rate .bpm').textContent = `${respRateAvg} bpm`
-    document.querySelector('.respiratory-rate .level').textContent = respRateLevel
-    
-    const tempAvg = Math.round(
-      filteredHistory.reduce((sum, item) => sum + item.temperature.value, 0) / filteredHistory.length
-    ).toFixed(1)
-    const tempLevel = calculateLevel(tempAvg, 'temperature')
-  
-    document.querySelector('#fahrenheit').innerHTML = `${tempAvg} <sup>0</sup> F`
-    document.querySelector('.temperature .level').innerHTML = tempLevel
-  
-    const heartAvg = Math.round(
-      filteredHistory.reduce((sum, item) => sum + item.heart_rate.value, 0) / filteredHistory.length
-    )
-    const heartLevel = calculateLevel(heartAvg, 'heart')
-    document.querySelector('.heart-rate .bpm').innerHTML = `${heartAvg} bpm`
-    console.log(document.querySelector('.heart-rate .level').textContent = heartLevel)
-}
+  const systolicLevel = calculateLevel(systolicAvg, "systolic");
+  const diastolicLevel = calculateLevel(diastolicAvg, "diastolic");
+
+  // Update the systolic and diastolic info
+  document.querySelector(".systolic .value").textContent = systolicAvg;
+  document.querySelector(".systolic .level").textContent = systolicLevel.level;
+  document.querySelector(".diastolic .value").textContent = diastolicAvg;
+  document.querySelector(".diastolic .level").textContent = diastolicLevel.level;
+
+  // update respiratory rate
+  const respRateAvg = Math.round(
+    respData.reduce((sum, value) => sum + value, 0) / respData.length
+  );
+  const respRateLevel = calculateLevel(respRateAvg, "respiratory");
+  document.querySelector(
+    ".respiratory-rate .bpm"
+  ).textContent = `${respRateAvg} bpm`;
+  document.querySelector(".respiratory-rate .level").textContent =
+    respRateLevel.level;
+
+  const tempAvg = Math.round(
+    tempData.reduce((sum, value) => sum + value, 0) / tempData.length
+  ).toFixed(1);
+  const tempLevel = calculateLevel(tempAvg, "temperature");
+
+  document.querySelector("#fahrenheit").innerHTML = `${tempAvg} <sup>0</sup> F`;
+  document.querySelector(".temperature .level").innerHTML = tempLevel.level;
+
+  const heartAvg = Math.round(
+    heartData.reduce((sum, value) => sum + value, 0) / heartData.length
+  );
+  const heartLevel = calculateLevel(heartAvg, "heart");
+  document.querySelector(".heart-rate .bpm").innerHTML = `${heartAvg} bpm`;
+  document.querySelector(".heart-rate .level").textContent = heartLevel.level;
+};
